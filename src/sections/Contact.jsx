eb99b2dd3185c1f85c47dc5c4b70795531c59b9c
Vishnu <vishnugowtham97@gmail.com>
2025-05-26 08:42:50 +0530

    navigation fixed

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -51,7 +51,7 @@ const Contact = () => {
     }
   };
   return (
-    <section className="relative flex items-center c-space section-spacing">
+    <section className="relative flex items-center c-space section-spacing" id="contact">
       <Particles
         className="absolute inset-0 -z-50"
         quantity={100}
